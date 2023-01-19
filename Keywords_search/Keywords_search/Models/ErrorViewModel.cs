10a924e1439a5ee85a6e93c3616efb2ffebd2fa8
Muhammad Salman Rafique <53379867+salmanrafiq5839@users.noreply.github.com>
2023-01-19 14:47:19 +0500

    project created

---
+++ Keywords_search/Keywords_search/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Keywords_search.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
