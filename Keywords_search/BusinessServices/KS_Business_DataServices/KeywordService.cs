2891dfa6653fc5a5bc74bb6e9207efc07d163b91
Muhammad Salman Rafique <53379867+salmanrafiq5839@users.noreply.github.com>
2023-02-20 12:09:40 +0500

    new model

---
+++ Keywords_search/BusinessServices/KS_Business_DataServices/KeywordService.cs
@@ -0,0 +1,21 @@
+﻿using KS_Business_Model;
+
+namespace KS_Business_DataServices
+{
+    public class KeywordService
+    {
+        private List<Keyword_Model> keyword = new List<Keyword_Model>();
+
+        public List<Keyword_Model> GetAll()
+        {
+            keyword.Add(new Keyword_Model { id = 1, keywords = "youtube" });
+            keyword.Add(new Keyword_Model { id = 2, keywords = "youtube search" });
+            keyword.Add(new Keyword_Model { id = 3, keywords = "youtube topic" });
+            keyword.Add(new Keyword_Model { id = 4, keywords = "youtube keywords" });
+            return keyword;
+        }
+        
+
+
+    }
+}
